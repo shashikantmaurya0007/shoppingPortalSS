c7ceffd27b3ca6e86e005f9267ddfc41dbe78fab
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-10-06 13:52:58 +0530

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; Copyright 2021</p>
+      <ul>
+        <li>
+          <a href="https://www.instagram.com/"> Insta </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Ffeed%2F">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/"> Facebook </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
